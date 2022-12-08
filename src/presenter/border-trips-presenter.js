e35d28433441901e24ce93e086316221f4025393
amsv <ammosovaleksey@yandex.ru>
2022-12-09 00:54:46 +0300

    +1.11. Время разбивать камни

--- src/presenter/border-trips-presenter.js
+++ src/presenter/border-trips-presenter.js
@@ -1,10 +1,12 @@
 import SortView from '../view/sort-view.js';
-import RouteView from '../view/route-view.js';
+import ListRouteView from '../view/list-route-view.js';
+import FormCreationTrip from '../view/form-creation-trip-view.js';
+import PointRoute from '../view/point-route.js';
+import FormEditTrip from '../view/form-edit-trip-view.js';
 import { render } from '../render.js';
 
 export default class BoardTripsPresenter {
-
-  routeListComponent = new RouteView();
+  routeListComponent = new ListRouteView();
 
   constructor({boardTripsContainer}) {
     this.boardTripsContainer = boardTripsContainer;
@@ -12,6 +14,13 @@ export default class BoardTripsPresenter {
 
   init() {
     render(new SortView(), this.boardTripsContainer);
+    render(new FormCreationTrip(), this.boardTripsContainer);
     render(this.routeListComponent, this.boardTripsContainer);
+    for (let i = 0; i < 3; i++) {
+      render(new PointRoute(), this.routeListComponent.getElement());
+      if( i === 0 ) {
+        render(new FormEditTrip(), this.routeListComponent.getElement());
+      }
+    }
   }
 }
