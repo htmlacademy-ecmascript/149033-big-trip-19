ff4c5372d1950d9d2896917ced0b1b31e5138627
amsv <ammosovaleksey@yandex.ru>
2022-12-09 23:28:06 +0300

    +наиминг: pageBodyElement

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import FilterView from './view/filter-view.js';
 import { render } from './render.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 
-const sitePageElement = document.querySelector('.page-body');
-const tripControlsFiltersElement = sitePageElement.querySelector('.trip-controls__filters');
-const tripEventsElement = sitePageElement.querySelector('.trip-events');
+const pageBodyElement = document.querySelector('.page-body');
+const tripControlsFiltersElement = pageBodyElement.querySelector('.trip-controls__filters');
+const tripEventsElement = pageBodyElement.querySelector('.trip-events');
 
 const boardTripsPresenter = new TripEventsPresenter({boardTripsContainer: tripEventsElement});
 render(new FilterView(), tripControlsFiltersElement);
