cf15593442b948d23ec4ba71635e18a7098b5128
amsv <ammosovaleksey@ya.ru>
2022-12-21 18:15:14 +0300

    исправили вывод массива points

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -4,19 +4,18 @@ import { getDestinations } from '../mock/destination.js';
 const LIMIT_POINT = 4;
 const destinationsList = getDestinations();
 const offersList = getOffers();
+const getPointsAddition = (item) => ({
+  ...item,
+  destination: destinationsList[item.destination],
+  offers: offersList[item.offers],
+});
+
 export default class PointsModel {
   constructor() {
     this.points = Array.from({length: LIMIT_POINT}, getRandomPoint);
-
   }
 
   getPoints() {
-    this.points.forEach( (item) => {
-      item.destination = destinationsList[item.destination];
-      item.offers = offersList[item.offers];
-    });
-    console.log(this.points);
-    return this.points;
+    return this.points.map( getPointsAddition );
   }
-
 }
