db54577cad450de83413a25b8ed2acbcada1c53d
amsv <ammosovaleksey@yandex.ru>
2023-02-06 01:09:37 +0300

    8.9. Дополнительная функциональность

---
+++ src/presenter/informer-presenter.js
@@ -0,0 +1,46 @@
+import InformerView from '../view/informer-view.js';
+import { remove, render, RenderPosition } from '../framework/render';
+import { sortPointUp } from '../utils/point.js';
+
+export default class InformerPresenter {
+  #informerContainer = null;
+  #informerComponent = null;
+  #pointsModel = null;
+  #offersModel = null;
+  #destinationsModel = null;
+  #points = [];
+  #offers = [];
+  #destinations = [];
+  #pointsWithDestinationsOffers = [];
+
+  constructor({informerContainer, pointsModel, offersModel , destinationsModel}) {
+    this.#informerContainer = informerContainer;
+    this.#pointsModel = pointsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
+
+    this.#points = this.#pointsModel.points;
+    this.#offers = this.#offersModel.offers;
+    this.#destinations = this.#destinationsModel.destinations;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
+  }
+
+  init = () => {
+    this.#points = this.#points.sort(sortPointUp);
+    this.#pointsWithDestinationsOffers = this.#pointsModel.getPointsWithDestinations({offers: this.#offers, destinations: this.#destinations});
+    this.#informerComponent = new InformerView({pointsWithDestinationsOffers: this.#pointsWithDestinationsOffers});
+
+    render(this.#informerComponent, this.#informerContainer, RenderPosition.AFTERBEGIN);
+  };
+
+  #handleModelEvent = () => {
+    remove(this.#informerComponent);
+
+    this.#points = this.#pointsModel.points;
+    this.#offers = this.#offersModel.offers;
+    this.#destinations = this.#destinationsModel.destinations;
+
+    this.init();
+  };
+}
