9e95efb4670f9d274515b7b5683620a0e3ec8198
amsv <ammosovaleksey@yandex.ru>
2023-02-09 00:20:39 +0300

    исправил: ошибка в названии константы MINUTS_IN_HOUR и isPesentPoint

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,10 @@
 import { FilterType } from '../const.js';
-import { isPastPoint, isFuturePoint, isPesentPoint } from '../utils/point.js';
+import { isPastPoint, isFuturePoint, isPresentPoint } from '../utils/point.js';
 
 const filter = {
   [FilterType.EVERYTHING]: (points) => points,
   [FilterType.FUTURE]: (points) => points.filter((point) => isFuturePoint(point.dateFrom)),
-  [FilterType.PRESENT]: (points) => points.filter((point) => isPesentPoint(point.dateFrom, point.dateTo)),
+  [FilterType.PRESENT]: (points) => points.filter((point) => isPresentPoint(point.dateFrom, point.dateTo)),
   [FilterType.PAST]: (points) => points.filter((point) => isPastPoint(point.dateTo)),
 };
 
